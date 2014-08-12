a773b382bd33e9c3e1f80eea6ee0b98fc326699e
gin0606 <kkgn06@gmail.com>
2014-08-12 19:07:22 +0900

    Add doc comment

--- common/WebView.h
+++ common/WebView.h
@@ -14,18 +14,44 @@ class WebViewImpl;
 
 class WebView : public cocos2d::ui::Widget {
 public:
+    /**
+    * Allocates and initializes a WebView.
+    */
     static WebView *create();
 
+    /**
+    * Default constructor
+    */
     WebView();
+
+    /**
+    * Default destructor
+    */
     virtual ~WebView();
 
+    /**
+    * Call before a web view begins loading.
+    * @param sender The web view that is about to load new content.
+    * @param url content URL.
+    * @return YES if the web view should begin loading content; otherwise, NO .
+    */
     std::function<bool(WebView *sender, std::string url)> shouldStartLoading;
+    /**
+    * Call after a web view finishes loading.
+    * @param sender The web view that has finished loading.
+    * @param url content URL.
+    */
     std::function<void(WebView *sender, std::string url)> didFinishLoading;
+    /**
+    * Call if a web view failed to load content.
+    * @param sender The web view that has failed loading.
+    * @param url content URL.
+    */
     std::function<void(WebView *sender, std::string url)> didFailLoading;
 
     /**
     * Set javascript interface scheme.
-    * See `onJsCallback`.
+    * @see #onJsCallback
     */
     void setJavascriptInterfaceScheme(const std::string &scheme);
 
@@ -50,7 +76,16 @@ public:
     */
     void loadHTMLString(const std::string &string, const std::string &baseURL);
 
+    /**
+    * Loads the given URL.
+    * @param url content URL
+    */
     void loadUrl(const std::string &url);
+
+    /**
+    * Loads the given fileName.
+    * @param fileName content fileName
+    */
     void loadFile(const std::string &fileName);
 
     /**
@@ -63,14 +98,31 @@ public:
     */
     void reload();
 
+    /**
+    * Gets whether this WebView has a back history item.
+    * @return web view has a back history item.
+    */
     bool canGoBack();
 
+    /**
+    * Gets whether this WebView has a forward history item.
+    * @return web view has a forward history item.
+    */
     bool canGoForward();
 
+    /**
+    * Goes back in the history.
+    */
     void goBack();
 
+    /**
+    * Goes forward in the history.
+    */
     void goForward();
 
+    /**
+    * evaluates JavaScript in the context of the currently displayed page
+    */
     void evaluateJS(const std::string &js);
 
     virtual void draw(cocos2d::Renderer *renderer, cocos2d::Mat4 const &transform, uint32_t flags) override;
